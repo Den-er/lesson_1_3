7ab820de97849e91a27485c9599f5ddd5dd3b93f
Den-er <Ieroffieiev@outlook.com>
2024-11-27 20:56:30 +0300

    Add files via upload

---
+++ lines.py
@@ -0,0 +1,6 @@
+example = "Классифицификус"
+print (example [0])
+print (example [-1])
+print (example[len(example)//2+1::]) # команда  len выдает сколько символов в слове, // целочисленное деление
+print (example [::-1])
+print (example [1::2])
